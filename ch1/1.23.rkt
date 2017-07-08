1fe9d7f78ecd0cf29193cf9cae597c2e886adae7
sjhuangx <sjhuangx@gmail.com>
2017-07-08 21:28:33 +0800

    add 1.22 to 1.26 exercise

---
+++ ch1/1.23.rkt
@@ -0,0 +1,21 @@
+#lang eopl
+
+(require "../utils.rkt")
+
+; Exercise 1.23
+
+(define list-index-ref
+    (lambda (pred lst ref)
+        (if (null? lst)
+            #f
+            (if (pred (car lst))
+                ref
+                (list-index-ref pred (cdr lst) (+ ref 1))))))
+
+(define list-index
+    (lambda (pred lst)
+        (list-index-ref pred lst 0)))
+
+(equal?? (list-index number? '(a 2 (1 3) b 7)) 1)
+(equal?? (list-index symbol? '(a (b c) 17 foo)) 0)
+(equal?? (list-index symbol? '(1 2 (a b) 3)) #f)
